721e9525d5ba046b19bd09e570022a8eac818f17
Documenter.jl <documenter@juliadocs.github.io>
2021-04-07 19:48:55 +0000

    build based on 6ed8ca8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DocThemeIndigo","category":"page"},{"location":"#DocThemeIndigo","page":"Home","title":"DocThemeIndigo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DocThemeIndigo]","category":"page"}]
+}
